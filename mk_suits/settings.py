5eb8828ceba95771a05529efa3f27b87a8f4be3f
atwanzireT <tatwanzire@gmail.com>
2025-05-16 14:16:08 +0300

    worked on static

--- mk_suits/settings.py
+++ mk_suits/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$_h+e-*%zteqo870ak_3wm*pft+u&q%^6i-ph$oy62!tm^==b%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -95,24 +95,24 @@ WSGI_APPLICATION = 'mk_suits.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mksuites',
-        'USER': 'postgres',
-        'PASSWORD': 'atwanzire',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'mksuites',
+#         'USER': 'postgres',
+#         'PASSWORD': 'atwanzire',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
@@ -152,15 +152,25 @@ USE_TZ = True
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/user/login/'
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # dev files
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # dev files
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
-# Media files (uploaded by users)
-MEDIA_URL = '/media/'
+# # Media files (uploaded by users)
+# MEDIA_URL = '/media/'
 
-# Directory to store uploaded media
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# # Directory to store uploaded media
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+STATIC_URL = 'static/'
+#STATICFILES_DIRS =['static/']
+STATIC_ROOT = BASE_DIR/'static'
+
+MEDIA_URL = 'media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = BASE_DIR/'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
