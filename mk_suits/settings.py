9c93909aef264b86e4949f4c67d583c82fa455e9
atwanzireT <tatwanzire@gmail.com>
2025-05-16 12:27:14 +0300

    updating the DB

--- mk_suits/settings.py
+++ mk_suits/settings.py
@@ -95,24 +95,24 @@ WSGI_APPLICATION = 'mk_suits.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'mksuites',
-#         'USER': 'postgres',
-#         'PASSWORD': 'atwanzire',
-#         'HOST': 'localhost',  # Or the IP of your DB server
-#         'PORT': '5432',        # Default PostgreSQL port
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mksuites',
+        'USER': 'postgres',
+        'PASSWORD': 'atwanzire',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
