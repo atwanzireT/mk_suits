2991ad33efbf74755bf7955e4d9137ef33aab6a4
ndagirelillian <ndagirelillian15@gmail.com>
2025-06-05 15:15:13 +0300

    staticfiles wrong

--- mk_suits/settings.py
+++ mk_suits/settings.py
@@ -160,16 +160,14 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/user/login/'
 
 
-
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 STATIC_URL = 'static/'
-# STATICFILES_DIRS = [
-#      os.path.join(BASE_DIR, 'static'),
-#  ]
-STATIC_ROOT = BASE_DIR/'static'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_ROOT = BASE_DIR/'staticfiles'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = 'media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR,'media')
