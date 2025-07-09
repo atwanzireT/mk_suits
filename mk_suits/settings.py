ab7e82c7be884a3e0fa9ecb5221446f3950a2c55
ndagirelillian <ndagirelillian15@gmail.com>
2025-07-09 10:02:53 +0300

    changes in static

--- mk_suits/settings.py
+++ mk_suits/settings.py
@@ -53,7 +53,6 @@ INSTALLED_APPS = [
 ]
 
 
-
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
@@ -77,7 +76,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             BASE_DIR / 'templates',
-            ],
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -154,10 +153,7 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/user/login/'
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-
-#     os.path.join(BASE_DIR, 'static'),
-# ]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # dev files
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Media files (uploaded by users)
@@ -170,9 +166,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-
-
-
-
